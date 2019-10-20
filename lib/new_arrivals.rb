a18836f9d4debddb2115540fb76cd7f3197c520a
Jharell Chambers <jharell.chambers@selu.edu>
2019-10-20 21:10:40 +0000

    first commit

---
+++ lib/new_arrivals.rb
@@ -0,0 +1,6 @@
+require "new_arrivals/version"
+
+module NewArrivals
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/new_arrivals/version.rb
@@ -0,0 +1,3 @@
+module NewArrivals
+  VERSION = "0.1.0"
+end
